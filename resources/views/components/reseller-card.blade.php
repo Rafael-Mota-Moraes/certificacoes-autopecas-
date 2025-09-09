667451cdb83c157d27c77e6be64c3da36dbe377c
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-09-08 21:38:04 -0300

    style: change system font

--- resources/views/components/reseller-card.blade.php
+++ resources/views/components/reseller-card.blade.php
@@ -3,16 +3,17 @@
 <div class="bg-white shadow-lg overflow-hidden flex flex-col flex-shrink-0 w-80 ">
     <div class="p-4 flex flex-col flex-grow ">
         <div class="flex items-end justify-between mb-2">
-            <span class="text-lg font-bold ml-auto mx-2">{{ number_format($reseller->reviews_avg_rating, 1, ',', '') }}</span>
-            <img src="images/{{ceil($reseller->reviews_avg_rating)}}-star.svg" alt="">
+            <span
+                class="text-lg font-bold ml-auto mx-2">{{ number_format($reseller->reviews_avg_rating, 1, ',', '') }}</span>
+            <img src="images/{{ ceil($reseller->reviews_avg_rating) }}-star.svg" alt="">
         </div>
 
 
         <img class="w-full h-40 object-cover" src="{{ $reseller->image_url ?? 'images/car-placeholder.png' }}"
-             alt="Foto da {{ $reseller->name }}">
+            alt="Foto da {{ $reseller->name }}">
 
 
-        <p class="mb-4 font-bold text-xl">
+        <p class="my-2 font-bold text-xl">
             {{ $reseller->name }}
         </p>
 
@@ -26,12 +27,12 @@
 
         <div class="flex justify-center items-center">
             <a href="#"
-               class="mt-auto block w-25 bg-[#840032] text-white text-center font-semibold py-2 px-4 rounded-md hover:bg-[#6a0028] transition-colors">
+                class="mt-auto block w-25 bg-[#840032] text-white text-center font-semibold py-2 px-4 rounded-md hover:bg-[#6a0028] transition-colors">
                 Avaliar
             </a>
             <a href="#"
-               class="mt-auto block w-25 mx-2 text-center font-semibold py-2 px-4 rounded-md transition-colors border-1"
-               style="color: #840032;">
+                class="mt-auto block w-25 mx-2 text-center font-semibold py-2 px-4 rounded-md transition-colors border-1"
+                style="color: #840032;">
                 Ver mais
             </a>
         </div>
